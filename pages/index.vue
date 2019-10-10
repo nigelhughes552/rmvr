2d87f2497b54a6f6776a61766c0ecbe58de11af7
nigelhughes552 <nigel@theoldpo.plus.com>
2019-10-10 12:46:35 +0100

    edit flex

--- pages/index.vue
+++ pages/index.vue
@@ -1,84 +1,38 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm10 md10 lg10>
-      <section>
-        <h1>About</h1>
-        <p>
-          With over 30 years of experience in all makes and types of vehicles
-          R.M.V.R will help you. dfhdf fdh dfh fh fdhg df hfh fh dfh fdhg fdh
-          dfhfdh fh fh fh dfh
-        </p>
-      </section>
-      <section>
-        <h1>Location</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices
-          sagittis orci a scelerisque. Molestie at elementum eu facilisis sed
-          odio. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare.
-          Commodo odio aenean sed adipiscing diam donec adipiscing. Turpis
-          egestas sed tempus urna et pharetra pharetra massa. Id eu nisl nunc mi
-          ipsum faucibus vitae aliquet. Ultrices gravida dictum fusce ut
-          placerat orci. Tellus at urna condimentum mattis pellentesque id.
-          Cursus eget nunc scelerisque viverra mauris in. Porta non pulvinar
-          neque laoreet suspendisse.
-        </p>
-      </section>
-
-      <v-parallax height="400" src="engine.jpg"></v-parallax>
-
-      <section>
-        <h1>What we do</h1>
-
-        <p>
-          nisl suscipit adipiscing bibendum. Gravida arcu ac tortor dignissim
-          convallis aenean et tortor. Leo integer malesuada nunc vel risus
-          commodo viverra maecenas accumsan. Sem et tortor consequat id porta
-          nibh venenatis cras. Congue nisi vitae suscipit tellus mauris a diam.
-          Porttitor rhoncus dolor purus non enim praesent elementum facilisis
-          leo. Justo eget magna fermentum iaculis eu non diam phasellus. Ornare
-          quam viverra orci sagittis eu volutpat odio. Sollicitudin ac orci
-          phasellus egestas tellus. Mi quis hendrerit dolor magna eget. Ut etiam
-          sit amet nisl purus in mollis nunc sed. Venenatis urna cursus eget
-          nunc scelerisque viverra mauris in. Amet porttitor eget dolor morbi.
-          Fermentum leo vel orci porta.
-        </p>
-      </section>
+  <v-layout row justify-center align-center>
+    <v-flex xs10>
+      <h1>About</h1>
       <p>
-        Fermentum posuere urna nec tincidunt praesent semper feugiat nibh.
-        Sodales neque sodales ut etiam sit amet nisl. Congue nisi vitae suscipit
-        tellus mauris a diam. Facilisis leo vel fringilla est ullamcorper eget
-        nulla facilisi etiam. Augue neque gravida in fermentum et sollicitudin
-        ac orci phasellus. Elit scelerisque mauris pellentesque pulvinar
-        pellentesque habitant morbi tristique. Faucibus turpis in eu mi
-        bibendum. Pulvinar etiam non quam lacus. Sed cras ornare arcu dui
-        vivamus. Morbi enim nunc faucibus a pellentesque sit amet. Eu non diam
-        phasellus vestibulum lorem sed. Donec massa sapien faucibus et molestie
-        ac feugiat sed lectus. Dictum fusce ut placerat orci nulla pellentesque.
-        Mattis pellentesque id nibh tortor. Nibh cras pulvinar mattis nunc sed
-        blandit. Porttitor lacus luctus accumsan tortor posuere ac ut consequat
-        semper. Sit amet consectetur adipiscing elit duis. Scelerisque varius
-        morbi enim nunc faucibus a pellentesque sit amet. Est sit amet facilisis
-        magna. Dictum at tempor commodo ullamcorper a lacus vestibulum. Et
-        pharetra pharetra massa massa ultricies. Nisi est sit amet facilisis
-        magna etiam tempor orci eu. Eu sem integer vitae justo eget magna
-        fermentum. Tincidunt praesent semper feugiat nibh sed pulvinar proin.
-        Nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Eu sem
-        integer vitae justo eget magna fermentum iaculis. Quis eleifend quam
-        adipiscing vitae proin sagittis nisl rhoncus mattis. Nisi vitae suscipit
-        tellus mauris. Auctor elit sed vulputate mi sit amet mauris commodo.
-        Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula.
-        Porttitor lacus luctus accumsan tortor. Massa enim nec dui nunc mattis
-        enim ut tellus elementum. Eu turpis egestas pretium aenean pharetra
-        magna ac placerat. Ut porttitor leo a diam. Pharetra vel turpis nunc
-        eget lorem dolor sed viverra ipsum. Accumsan in nisl nisi scelerisque eu
-        ultrices vitae auctor eu. Nisi scelerisque eu ultrices vitae auctor eu
-        augue. Interdum velit laoreet id donec. Posuere lorem ipsum dolor sit
-        amet. Nibh nisl condimentum id venenatis a condimentum. Ac auctor augue
-        mauris augue neque gravida in fermentum et. Sed enim ut sem viverra
-        aliquet eget. Ut aliquam purus sit amet luctus venenatis. Purus viverra
-        accumsan in nisl
+        With over 30 years of experience in all makes and types of vehicles
+        R.M.V.R will help you. dfhdf fdh dfh fh fdhg df hfh fh dfh fdhg fdh
+        dfhfdh fh fh fh dfh
       </p>
+    </v-flex>
+    <v-flex xs12>
+      <v-parallax src="car.jpg"></v-parallax>
+    </v-flex>
+
+    <v-flex xs10>
+      <h1>What we do</h1>
+      <p>
+        With over 30 years of experience in all makes and types of vehicles
+        R.M.V.R will help you. dfhdf fdh dfh fh fdhg df hfh fh dfh fdhg fdh
+        dfhfdh fh fh fh dfh
+      </p>
+    </v-flex>
+    <v-flex xs12>
+      <v-parallax src="engine.jpg"></v-parallax>
+    </v-flex>
+    <v-flex xs10>
+      <h1>About</h1>
+      <p>
+        With over 30 years of experience in all makes and types of vehicles
+        R.M.V.R will help you. dfhdf fdh dfh fh fdhg df hfh fh dfh fdhg fdh
+        dfhfdh fh fh fh dfh
+      </p>
+    </v-flex>
+    <v-flex xs12>
+      <v-parallax src="car.jpg"></v-parallax>
     </v-flex>
   </v-layout>
 </template>
